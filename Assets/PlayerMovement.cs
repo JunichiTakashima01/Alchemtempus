ab46be22e886c5b75dd8508152eb0aec6b922388
SchwiDolaUwU <junichi910839539@gmail.com>
2025-05-27 10:04:25 -0700

    enable double jump
    Limit the number of jump in the air

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,32 +1,62 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     public Rigidbody2D rb;
-    [Header("Movement")]
     public float moveSpeed = 5f;
 
-    [Header("Movement")]
     public float jumpPower = 10f;
+    public int maxJumps = 2;
+    private int jumpRemaining;
+
+    public Transform groundCheckPos;
+    public Vector2 groundCheckSize = new Vector2(0.5f, 0.05f);
+    public LayerMask groundLayer;
     public float horizontalMovement;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        jumpRemaining = 0;
     }
     // Update is called once per frame
     void Update()
     {
         rb.linearVelocity = new Vector2(horizontalMovement * moveSpeed, rb.linearVelocity.y);
+        GroundCheck();
     }
-    public void Move(InputAction.CallbackContext context){
+    public void Move(InputAction.CallbackContext context)
+    {
         horizontalMovement = context.ReadValue<Vector2>().x;
     }
 
-    public void Jump(InputAction.CallbackContext context){
-        if (context.performed){
-            rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (jumpRemaining  > 0)
+        {
+            if (context.performed)
+            {
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpPower);
+                jumpRemaining--;
+            }
         }
+    }
+
+    private void GroundCheck()
+    {
+        if (Physics2D.OverlapBox(groundCheckPos.position, groundCheckSize, 0, groundLayer))
+        {
+            if (rb.linearVelocity.y == 0)
+            {
+                jumpRemaining = maxJumps;
+            }
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawCube(groundCheckPos.position, groundCheckSize);
     }
 }
