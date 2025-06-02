494a12b14ef499600bf233713d0f03bfde1ed84a
SchwiDolaUwU <junichi910839539@gmail.com>
2025-06-01 20:19:32 -0700

    Updated ground check

--- Assets/Scripts/GroundCheckCollider.cs
+++ Assets/Scripts/GroundCheckCollider.cs
@@ -4,14 +4,21 @@ using UnityEngine;
 public class GroundCheckCollider : MonoBehaviour
 {
     public static event Action<bool> OnTouchingGround;
+    private int collisionObjectCount = 0;
 
     void OnTriggerEnter2D(Collider2D collision)
     {
+        collisionObjectCount += 1;
         OnTouchingGround.Invoke(true);
     }
 
     void OnTriggerExit2D(Collider2D collision)
     {
-        OnTouchingGround.Invoke(false);
+        collisionObjectCount -= 1;
+        if (collisionObjectCount == 0)
+        {
+            OnTouchingGround.Invoke(false);
+        }
     }
+
 }
