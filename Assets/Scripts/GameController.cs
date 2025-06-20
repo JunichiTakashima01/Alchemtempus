6624284a0a2163803479eadcc14ab54d6f775b95
SchwiDolaUwU <junichi910839539@gmail.com>
2025-06-19 21:58:33 -0700

    Implemented "Exit" button

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -25,4 +25,12 @@ public class GameController : MonoBehaviour
         Time.timeScale = 1;
     }
 
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+        Application.Quit();
+    }
+
 }
